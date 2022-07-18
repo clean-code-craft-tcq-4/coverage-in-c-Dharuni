15ccf9ad3033645601249a75159fd35af9abe303
Belavatagi-Dharuni <34068379+Dharuni@users.noreply.github.com>
2022-07-18 22:33:01 +0530

    Update typewise-alert.h

--- typewise-alert.h
+++ typewise-alert.h
@@ -40,6 +40,6 @@ typedef struct {
 void checkAndAlert(
   AlertTarget alertTarget, BatteryCharacter batteryChar, double temperatureInC);
 
-void sendToController(BreachType breachType);
-void sendToEmail(BreachType breachType);
+int sendToController(BreachType breachType);
+int sendToEmail(BreachType breachType);
 TemperatureLimit checkTempLimits(CoolingType coolingType)
